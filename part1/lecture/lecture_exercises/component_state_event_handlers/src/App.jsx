1c878f93abcd6ef26f43672da772ba4abc2066f2
AmirAttoun <amir.attoun@protonmail.ch>
2024-05-31 12:13:28 +0200

    modularized button and display

--- part1/lecture/lecture_exercises/component_state_event_handlers/src/App.jsx
+++ part1/lecture/lecture_exercises/component_state_event_handlers/src/App.jsx
@@ -1,25 +1,44 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-
-  const handleClick = () => {
-    console.log('clicked')
-  }
-
+const Display = (props) => {
   return (
-    <div>
-      <div>{counter}</div>
-
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero      
-      </button>
-    </div>
+    <div>{props.counter}</div>
   )
 }
 
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}/>
+
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
+    </div>
+  )
+}
 export default App
