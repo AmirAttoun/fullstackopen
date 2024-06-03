74706a46f25e3b9ee40c2495faa4bc52ca122843
AmirAttoun <amir.attoun@protonmail.ch>
2024-06-03 16:18:25 +0200

    Exercise

--- part1/exercises/unicafe/src/App.jsx
+++ part1/exercises/unicafe/src/App.jsx
@@ -1,16 +1,65 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticsLine = ({ text, value, total, type }) => 
+  if (total === 0) {
+    return <p></p>
+  }
+
+  let displayValue = value;
+  
+  if (type === 'average') {
+    displayValue = (value / total);
+  } else if (type === 'positive') {
+    displayValue = ((value / total) * 100) + '%';
+  }
+
+  return <p>{text} {displayValue}</p>
+}
+
+const Header = ({ text }) => <h1>{text}</h1>
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [totalClicks, setTotalClicks] = useState(0)
+
+  const handleGoodClick = () => {
+    setTotalClicks(totalClicks + 1)
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setTotalClicks(totalClicks + 1)
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setTotalClicks(totalClicks + 1)
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      code here
+      <Header text='give feedback' />
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <Header text='statistics' />
+      <StatisticsLine text='good' value={good} total={totalClicks} />
+      <StatisticsLine text='neutral' value={neutral} total={totalClicks} />
+      <StatisticsLine text='bad' value={bad} total={totalClicks} />
+      <StatisticsLine text='total clicks' value={totalClicks} total={totalClicks} /> 
+      <StatisticsLine text='average' value={good - bad} total={totalClicks} type='average' /> 
+      <StatisticsLine text='positive' value={good} total={totalClicks} type='positive' /> 
     </div>
   )
 }
 
-export default App
+export default App;
