b14aee2a60a56e777525a292d398f6db125e878f
AmirAttoun <amir.attoun@protonmail.ch>
2024-06-03 08:41:29 +0200

    exercises

--- part1/lecture/lecture_exercises/component_state_event_handlers/src/App.jsx
+++ part1/lecture/lecture_exercises/component_state_event_handlers/src/App.jsx
@@ -1,16 +1,9 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{props.counter}</div>
-  
-
-const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>
-  
-
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
-
   const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
