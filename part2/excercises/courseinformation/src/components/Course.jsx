884bef3a615ccdf746aa9196e68ba4ad97d9107c
AmirAttoun <amir.attoun@protonmail.ch>
2024-06-10 16:43:49 +0200

    part2 additions

---
+++ part2/excercises/courseinformation/src/components/Course.jsx
@@ -0,0 +1,33 @@
+const Course = ({ course }) => {
+
+    const Header = ({ course }) => {
+      return <h1>{course.name}</h1>
+    }
+  
+    const Part = ({ part }) => {
+      return <p>{part.name} {part.exercises}</p>
+    }
+      
+    const Content = ({ course }) => {
+      return (
+        <div>
+          {course.parts.map(part => <Part key={part.id} part={part} />)}
+        </div>
+      )
+    }
+  
+    const Total = ({ course }) => {
+      const total = course.parts.reduce((acc, part) => acc + part.exercises, 0)
+      return <p><strong>total of {total} exercises</strong></p>
+    }
+  
+    return (
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    )
+  }
+
+  export default Course;
