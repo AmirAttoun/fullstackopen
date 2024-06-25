1ff8f11fcaa3d18db53b60fb9409eb5ef1b92f59
AmirAttoun <amir.attoun@protonmail.ch>
2024-06-25 15:11:30 +0200

    App.jsx

--- part2/lecture/lecture_exercises/collections/src/App.jsx
+++ part2/lecture/lecture_exercises/collections/src/App.jsx
@@ -27,10 +27,9 @@ const App = () => {
     };
 
     axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      console.log(response);
+      setNotes(notes.concat(response.data));
+      setNewNote("");
     });
-    //setNotes(notes.concat(noteObject));
-    //setNewNote("");
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
