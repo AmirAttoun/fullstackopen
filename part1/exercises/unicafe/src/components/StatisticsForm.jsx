51c079f9f5b5c68719c748813b3bb7199dcda00e
AmirAttoun <amir.attoun@protonmail.ch>
2025-03-24 21:35:17 +0100

    Completed exercise 1.6

--- part1/exercises/unicafe/src/components/StatisticsForm.jsx
+++ part1/exercises/unicafe/src/components/StatisticsForm.jsx
@@ -1,11 +1,27 @@
 const StatisticsForm = ({ good, neutral, bad }) => {
+  const calcAll = good + neutral + bad;
+  const calcAverage = (good - bad) / calcAll;
+  const calcPositive = (good / calcAll) * 100;
+
   return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-    </div>
+    <>
+      <div>
+        <h1>statistics</h1>
+        <p>
+          good {good}
+          <br />
+          neutral {neutral}
+          <br />
+          bad {bad}
+          <br />
+          all {calcAll}
+          <br />
+          average {calcAverage}
+          <br />
+          positive {calcPositive}%
+        </p>
+      </div>
+    </>
   );
 };
 
